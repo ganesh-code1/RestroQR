b669c08246e3a66da3aaa6a7654d9590cf2c19af
ganesh-6 <ganesh.22010331@viit.ac.in>
2025-03-12 21:32:19 +0530

    sfd

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,11 +1,20 @@
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import cors from "cors";
 
 export const sessionMiddleware = session({
   secret: "firstsession",
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false },
+  store: MongoStore.create({
+    mongoUrl: "mongodb+srv://hotel-admin:yourpassword@restroqr.mkvof.mongodb.net/RestroQR?retryWrites=true&w=majority",
+    collectionName: "sessions",
+  }),
+  cookie: {
+    secure: true, 
+    httpOnly: true,
+    sameSite: "None",
+  },
 });
 
 export const corsMiddleware = cors({
