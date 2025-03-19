960e58ba6db7800c4461c8de6dc6ab224e62128b
ganesh-6 <ganesh.22010331@viit.ac.in>
2025-03-19 16:24:02 +0530

    qr2

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -13,7 +13,7 @@ export const sessionMiddleware = session({
 });
 
 export const corsMiddleware = cors({
-  origin: ["https://restro-qr.vercel.app"],
+  origin: ["https://restro-qr2.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
